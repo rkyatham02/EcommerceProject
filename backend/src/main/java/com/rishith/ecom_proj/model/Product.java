626b8caf8065cbd56c0fa6fade544567fb47b278
rkyatham02 <rkyatham02@gmail.com>
2024-12-31 15:09:57 -0500

    Project setup using Spring

---
+++ backend/src/main/java/com/rishith/ecom_proj/model/Product.java
@@ -0,0 +1,28 @@
+package com.rishith.ecom_proj.model;
+import java.math.BigDecimal;
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data; // for automatically creating getters and setters
+import lombok.NoArgsConstructor;
+
+@Entity // JPA needs entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id // setting primary key
+    private int id;
+    private String name;
+    private String desc;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean available;
+    private int quantity;
+
+}
